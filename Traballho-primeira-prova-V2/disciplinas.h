51ae02fa597d2a0fc466e00a0f764d21ce69d13c
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-09-18 10:28:07 -0300

    as

--- Traballho-primeira-prova-V2/disciplinas.h
+++ Traballho-primeira-prova-V2/disciplinas.h
@@ -1,12 +1,17 @@
-
+#ifndef DISCIPLINAS_H
+#define DISCIPLINAS_H
 typedef struct Arvore_disciplinas {
     int codigo;
     char nome[50];
-    int periodo;
     int carga_horaria;
-    struct arvore_disciplinas *esq;
-    struct arvore_disciplinas *dir;
+    int periodo;
+    struct Arvore_disciplinas *esq;
+    struct Arvore_disciplinas *dir;
 }arvore_disciplinas;
 
 arvore_disciplinas *criar_disciplina();
 arvore_disciplinas *inserir_disciplina(arvore_disciplinas *raiz, arvore_disciplinas *no);
+arvore_disciplinas *buscar_disciplina(arvore_disciplinas *raiz, int codigo);
+arvore_disciplinas *remover_disciplina(arvore_disciplinas *raiz, int codigo);
+void imprimir_disciplinas(arvore_disciplinas *raiz);
+#endif // DISCIPLINAS_H
