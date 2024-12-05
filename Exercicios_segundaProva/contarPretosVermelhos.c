845e292971a1867774b849e116394c979a925e04
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-12-04 22:48:15 -0300

    as

--- Exercicios_segundaProva/contarPretosVermelhos.c
+++ Exercicios_segundaProva/contarPretosVermelhos.c
@@ -23,6 +23,66 @@ int contarNodosCores(Tree *arv, int *red, int *black) {
     // Chamar recursivamente para os filhos esquerdo e direito
     contarNodosCores(arv->esq, red, black);
     contarNodosCores(arv->dir, red, black);
+
+    return 0;
+}
+
+//1.Faça uma função que retorne por função a quantidade de elementos pretos da árvore vermelho e preta
+void contarPretos(Tree *arv, int *black) {
+    if (arv) {
+        if (arv->cor == 1) {
+            (*black)++;
+        }
+        contarPretos(arv->esq, black);
+        contarPretos(arv->dir, black);
+    }
+}
+
+//2.Faça uma função que retorne por parâmetro a soma dos múltiplos de 5 que são da cor vermelha e por função o .produto dos múltiplos de 5 que são da cor preta da árvore vermelho e preta
+
+int somarMultiplos5(Tree *arv, int *soma) {
+    int produto = 1;
+
+    if (arv) {
+        if(arv->cor == 0 && arv->info % 5 == 0) {
+            *soma = *soma + arv->info;
+        }
+
+        if(arv->cor == 1 && arv->info % 5 == 0) {
+            produto = produto * arv->info;
+        }
+
+        produto *= somarMultiplos5(arv->esq, soma);
+        produto *= somarMultiplos5(arv->dir, soma);
+    }
+
+    return produto;
+}
+
+//4.Faça uma função que, dado um valor, retorne por parâmetro a quantidade de ascendentes (caso o nó seja preto) e a quantidade de descendentes (caso o nó seja vermelho)
+
+void contarAscDesc(Tree *arv, int valor, int *asc, int *desc) {
+    if (arv) {
+        if (arv->info == valor) {
+            if (arv->cor == 1) {
+                *asc = contarNos(arv->dir);
+            }
+            else {
+                *desc = contarNos(arv->esq);
+            }
+        contarAscDesc(arv->esq, valor, asc, desc);
+        contarAscDesc(arv->dir, valor, asc, desc);
+        }
+    }
+}
+
+int contarNos (Tree *arv ) {
+    int qtdNos = 0;
+     if (arv) {
+        qtdNos++;
+        qtdNos += contarNos(arv->esq);
+        qtdNos += contarNos(arv->dir);
+     }
 }
 
 int main() {
